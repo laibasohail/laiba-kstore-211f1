2231a30d9eea80f89cff68e1e2b9a9389cb88d5d
laibasohail <105979681+laibasohail@users.noreply.github.com>
2022-10-08 17:52:58 +0500

    Add files via upload

---
+++ kstore/admin/add-user.php
@@ -0,0 +1,96 @@
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</head>
+
+
+<?php include "header.php"; ?>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="admin-heading">Add User</h1>
+            </div>
+        
+
+
+
+            <div class="col-md-offset-3 col-md-6">
+                <!-- Form Start -->
+                <form action="" method="POST" autocomplete="off">
+                    <div class="form-group">
+                        <label>First Name</label>
+                        <input type="text" name="fname" class="form-control" placeholder="First Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>Last Name</label>
+                        <input type="text" name="lname" class="form-control" placeholder="Last Name" required>
+                    </div>
+                    <div class="form-group">
+                        <label>User Name</label>
+                        <input type="text" name="user" class="form-control" placeholder="Username" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" placeholder="Password" required>
+                    </div>
+                    <div class="form-group">
+                        <label>User Role</label>
+                        <select class="form-control" name="role">
+                            <option value="0">Normal User</option>
+                            <option value="1">Admin</option>
+                        </select>
+                    </div>
+                    <input type="submit" name="save" class="btn btn-primary" value="Save" required />
+                </form>
+                <!-- Form End-->
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+if(isset($_POST["save"]))
+{
+    $user_fname = $_POST["fname"];
+$user_lname = $_POST["lname"];
+$user_name = $_POST["user"];
+$user_password = md5($_POST["password"]);
+$user_role = $_POST["role"];
+
+include "config.php";
+
+$query = "SELECT * FROM `user` WHERE username = '{$user_name}'";
+
+$result  = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result)>0)
+{
+    echo "user alraedy exist";
+
+}
+else
+{
+    $query1 = "INSERT INTO `user`(`first_name`, `last_name`, `username`, `password`, `role`) VALUES ('{$user_fname}','{$user_lname}','{$user_name}','{$user_password}','{$user_role}')";
+
+    mysqli_query($conn, $query1);
+
+   header("Location:http://localhost:82/kj/admin/users.php");
+
+    
+
+}
+
+
+
+
+
+
+}
+
+
+?>
+<?php include "footer.php"; ?>
---
+++ kstore/admin/logout.php
@@ -0,0 +1,13 @@
+<?php 
+
+
+session_start();
+session_unset();
+session_destroy();
+
+
+
+header("location:http://localhost:82/kstore_2111b1/admin/index.php");
+
+
+?>
